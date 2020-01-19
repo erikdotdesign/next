2bb9e0eaa460d25737f9e98979e7db866ab326d2
Erik <erik@Eriks-MBP.attlocal.net>
2020-01-19 13:07:23 -0500

    removed shape styles that would be applied on the svg

--- ts/resources/ui/styles/shapeStyles.ts
+++ ts/resources/ui/styles/shapeStyles.ts
@@ -1,11 +1,22 @@
 import * as styles from './layerStyles';
+import {styleReducer } from '../utils';
 
 const shapeStyles = (layer: srm.Shape | srm.ShapePath) => {
-  const baseStyles = styles.createBaseLayerStyles(layer);
-
-  return {
-    ...baseStyles
-  }
+  const { frame } = layer;
+  // generate styles
+  const width = styles.createWidth(frame.width);
+  const height = styles.createHeight(frame.height);
+  const top = styles.createTop(frame.y);
+  const left = styles.createLeft(frame.x);
+  // combine styles
+  const combined = [
+    width,
+    height,
+    top,
+    left
+  ];
+  // return final style object
+  return styleReducer(combined);
 };
 
 export default shapeStyles;
