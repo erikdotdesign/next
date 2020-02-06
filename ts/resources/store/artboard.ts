58e46b17df3fd012c1816d9d6cf0c59493dc91fb
Erik <erik@Eriks-MBP.attlocal.net>
2020-02-06 16:21:46 -0500

    yes no maybe so

--- ts/resources/store/artboard.ts
+++ ts/resources/store/artboard.ts
@@ -89,54 +89,102 @@ const getMaskShape = (layer: srm.SketchLayer): srm.Shape | srm.ShapePath => {
   return lastLayer as srm.ShapePath | srm.Shape;
 };
 
-const createMaskGroups = (page: srm.Page, layers: srm.SketchLayer[], sketch: srm.Sketch): void => {
-  if (layers.length > 0) {
-    layers.forEach((layer: srm.SketchLayer) => {
-      const hasClippingMask: boolean = layer.sketchObject.hasClippingMask();
-      if (hasClippingMask) {
-        const maskIndex = layer.index;
-        const maskParent = layer.parent;
-        // get mask shape
-        const maskShape = getMaskShape(layer);
-        // flatten shape if polygon, star, or triangle
-        const flatMaskShape = createMaskLayer(maskShape as srm.Shape | srm.ShapePath, sketch);
-        // add prefix to name
-        // add offset to group if flat mask shape if slimmer than mask shape
-        const maskGroupOffset = flatMaskShape.frame.width !== maskShape.frame.width
-                                ? (maskShape.frame.width - flatMaskShape.frame.width) / 2
-                                : maskShape.frame.x;
-        // create new group to mimic mask behavior
-        // app will apply overflow hidden to groups with the name srm.mask
-        const maskGroup = new sketch.Group({
-          name: 'srm.mask',
-          frame: {
-            ...flatMaskShape.frame,
-            x: maskGroupOffset
-          },
-          layers: [flatMaskShape]
-        });
-        // splice in mask group, splice out old mask
-        maskParent.layers.splice(maskIndex, 1, maskGroup);
-        // if mask is a group, push group layers to mask group
-        if (layer.type === 'Group') {
-          (layer as srm.Group).layers.forEach((maskedLayer: srm.SketchLayer) => {
-            maskGroup.layers.push(maskedLayer);
-          });
-        }
-        // loop through mask parent layers,
-        // any layer with an index higher than the mask will be masked
-        // push masked layers to maskGroup
-        maskParent.layers.forEach((maskedLayer: srm.SketchLayer, index: number) => {
-          if (index > maskIndex) {
-            maskedLayer.frame.x = maskedLayer.frame.x - maskGroup.frame.x;
-            maskedLayer.frame.y = maskedLayer.frame.y - maskGroup.frame.y;
-            maskGroup.layers.push(maskedLayer);
-          }
-        });
-      } else if (layer.type === "Group") {
-        createMaskGroups(page, (<srm.Group>layer).layers, sketch);
+// const createMaskGroups = (page: srm.Page, layers: srm.SketchLayer[], sketch: srm.Sketch): void => {
+//   if (layers.length > 0) {
+//     layers.forEach((layer: srm.SketchLayer) => {
+//       const hasClippingMask: boolean = layer.sketchObject.hasClippingMask();
+//       if (hasClippingMask) {
+//         const maskIndex = layer.index;
+//         const maskParent = layer.parent;
+//         // get mask shape
+//         const maskShape = getMaskShape(layer);
+//         // flatten shape if polygon, star, or triangle
+//         const flatMaskShape = createMaskLayer(maskShape as srm.Shape | srm.ShapePath, sketch);
+//         // add prefix to name
+//         // add offset to group if flat mask shape if slimmer than mask shape
+//         const maskGroupOffset = flatMaskShape.frame.width !== maskShape.frame.width
+//                                 ? (maskShape.frame.width - flatMaskShape.frame.width) / 2
+//                                 : maskShape.frame.x;
+//         // create new group to mimic mask behavior
+//         // app will apply overflow hidden to groups with the name srm.mask
+//         const maskGroup = new sketch.Group({
+//           name: 'srm.mask',
+//           frame: {
+//             ...flatMaskShape.frame,
+//             x: maskGroupOffset
+//           },
+//           layers: [flatMaskShape]
+//         });
+//         // splice in mask group, splice out old mask
+//         maskParent.layers.splice(maskIndex, 1, maskGroup);
+//         // if mask is a group, push group layers to mask group
+//         if (layer.type === 'Group') {
+//           (layer as srm.Group).layers.forEach((maskedLayer: srm.SketchLayer) => {
+//             maskGroup.layers.push(maskedLayer);
+//           });
+//         }
+//         // loop through mask parent layers,
+//         // any layer with an index higher than the mask will be masked
+//         // push masked layers to maskGroup
+//         maskParent.layers.forEach((maskedLayer: srm.SketchLayer, index: number) => {
+//           if (index > maskIndex) {
+//             maskedLayer.frame.x = maskedLayer.frame.x - maskGroup.frame.x;
+//             maskedLayer.frame.y = maskedLayer.frame.y - maskGroup.frame.y;
+//             maskGroup.layers.push(maskedLayer);
+//           }
+//         });
+//       } else if (layer.type === "Group") {
+//         createMaskGroups(page, (<srm.Group>layer).layers, sketch);
+//       }
+//     });
+//   }
+// };
+
+const createMaskGroup = (layer: srm.SketchLayer, sketch: srm.Sketch, callback: any): void => {
+  if (layer.sketchObject.hasClippingMask()) {
+    const maskIndex = layer.index;
+    const maskParent = layer.parent;
+    // get mask shape
+    const maskShape = getMaskShape(layer);
+    // flatten shape if polygon, star, or triangle
+    const flatMaskShape = createMaskLayer(maskShape as srm.Shape | srm.ShapePath, sketch);
+    // add prefix to name
+    // add offset to group if flat mask shape if slimmer than mask shape
+    const maskGroupOffset = flatMaskShape.frame.width !== maskShape.frame.width
+                            ? (maskShape.frame.width - flatMaskShape.frame.width) / 2
+                            : maskShape.frame.x;
+    // create new group to mimic mask behavior
+    // app will apply overflow hidden to groups with the name srm.mask
+    const maskGroup = new sketch.Group({
+      name: 'srm.mask',
+      frame: {
+        ...flatMaskShape.frame,
+        x: maskGroupOffset
+      },
+      layers: [flatMaskShape]
+    });
+    // splice in mask group, splice out old mask
+    maskParent.layers.splice(maskIndex, 1, maskGroup);
+    // if mask is a group, push group layers to mask group
+    if (layer.type === 'Group') {
+      (layer as srm.Group).layers.forEach((maskedLayer: srm.SketchLayer) => {
+        maskGroup.layers.push(maskedLayer);
+      });
+    }
+    // loop through mask parent layers,
+    // any layer with an index higher than the mask will be masked
+    // push masked layers to maskGroup
+    maskParent.layers.forEach((maskedLayer: srm.SketchLayer, index: number) => {
+      if (index > maskIndex) {
+        maskedLayer.frame.x = maskedLayer.frame.x - maskGroup.frame.x;
+        maskedLayer.frame.y = maskedLayer.frame.y - maskGroup.frame.y;
+        maskGroup.layers.push(maskedLayer);
       }
     });
+    // return callback
+    return callback(layer);
+  } else {
+    return callback(layer);
   }
 };
 
@@ -151,16 +199,98 @@ const flattenGroups = (layers: srm.SketchLayer[]): void => {
   }
 };
 
-const roundFrameDimensions = (layers: srm.SketchLayer[]): void => {
+// const roundFrameDimensions = (layers: srm.SketchLayer[]): void => {
+//   if (layers.length > 0) {
+//     layers.forEach((layer: srm.SketchLayer) => {
+//       layer.frame.x = Math.round(layer.frame.x);
+//       layer.frame.y = Math.round(layer.frame.y);
+//       layer.frame.width = Math.round(layer.frame.width);
+//       layer.frame.height = Math.round(layer.frame.height);
+//       if (layer.type === "Group") {
+//         roundFrameDimensions((<srm.Group>layer).layers);
+//       }
+//     });
+//   }
+// };
+
+const roundFrameDimensions = (layer: srm.SketchLayer, callback: any): void => {
+  layer.frame.x = Math.round(layer.frame.x);
+  layer.frame.y = Math.round(layer.frame.y);
+  layer.frame.width = Math.round(layer.frame.width);
+  layer.frame.height = Math.round(layer.frame.height);
+  return callback(layer);
+};
+
+const isIrrelevantLayer = (layer: srm.SketchLayer): boolean => {
+  if (layer.type === 'HotSpot' || layer.type === 'Slice' || layer.type === 'Artboard') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isRelevantLayer = (layer: srm.SketchLayer, callback: any) => {
+  switch(layer.type) {
+    case 'Group':
+    case 'Shape':
+    case 'Image':
+    case 'ShapePath':
+    case 'Text':
+    case 'SymbolInstance':
+      return callback(layer);
+    default:
+      layer.remove();
+      return;
+  }
+};
+
+const isHiddenLayer = (layer: srm.SketchLayer, callback: any) => {
+  const isHidden = (<srm.Group | srm.Shape | srm.Image | srm.ShapePath | srm.Text | srm.SymbolInstance>layer).hidden;
+  if (!isHidden) {
+    return callback(layer);
+  } else {
+    layer.remove();
+    return;
+  }
+};
+
+const isSymbol = (layer: srm.SketchLayer, callback: any) => {
+  if (layer.type === 'SymbolInstance') {
+    const detachedGroup = (<srm.SymbolInstance>layer).detach({
+      recursively: true
+    });
+    return callback(detachedGroup);
+  } else {
+    return callback(layer);
+  }
+};
+
+const singlePass = (layers: srm.SketchLayer[], sketch: srm.Sketch): void => {
   if (layers.length > 0) {
     layers.forEach((layer: srm.SketchLayer) => {
-      layer.frame.x = Math.round(layer.frame.x);
-      layer.frame.y = Math.round(layer.frame.y);
-      layer.frame.width = Math.round(layer.frame.width);
-      layer.frame.height = Math.round(layer.frame.height);
-      if (layer.type === "Group") {
-        roundFrameDimensions((<srm.Group>layer).layers);
-      }
+      isRelevantLayer(layer, (step1: any) => {
+        isSymbol(step1, (step2: any) => {
+          if (step2.id === step1.id) {
+            singlePass(step2.layers, sketch);
+          } else {
+            isHiddenLayer(step2, (step3: any) => {
+              createMaskGroup(step3, sketch, (step4: any) => {
+                if (step4.type === 'Group') {
+                  singlePass(step4.layers, sketch);
+                } else {
+                  roundFrameDimensions(step4, (step5: any) => {
+                    if (step5.type === 'Group') {
+                      singlePass(step5.layers, sketch);
+                    } else {
+                      return;
+                    }
+                  });
+                }
+              });
+            });
+          }
+        });
+      });
     });
   }
 };
@@ -172,16 +302,17 @@ const getArtboard = (page: srm.Page, selectedArtboard: srm.Artboard, sketch: srm
   artboard.frame.x = 0;
   artboard.frame.y = 0;
   artboard.background.includedInExport = true;
+  singlePass(artboard.layers, sketch);
   // removes hotspots, slices, and artboards
-  removeIrrelevantLayers(artboard.layers);
-  // detach all symbols from artboard, returns layer groups
-  detatchSymbols(artboard.layers);
-  // remove hidden layers
-  removeHiddenLayers(artboard.layers);
-  // create mask groups
-  createMaskGroups(page, artboard.layers, sketch);
-  // round layer frame dimensions
-  roundFrameDimensions(artboard.layers);
+  // removeIrrelevantLayers(artboard.layers);
+  // // detach all symbols from artboard, returns layer groups
+  // detatchSymbols(artboard.layers);
+  // // remove hidden layers
+  // removeHiddenLayers(artboard.layers);
+  // // create mask groups
+  // createMaskGroups(page, artboard.layers, sketch);
+  // // round layer frame dimensions
+  // roundFrameDimensions(artboard.layers);
   // return final artboard
   return artboard;
 };
